16b22d16b563ed9e9c179aba47690ae6a9171290
Carlos Payá <carlos.paya@csic.es>
2024-02-01 12:23:34 +0100

    email updated

--- email_aux.py
+++ email_aux.py
@@ -8,8 +8,8 @@ def write_text(name, guardian_name, center_cat):
                             <br> Queremos confirmarte que la inscripción a la Prepascua 2024 (15 a 17 de marzo) de {n} ha sido realizada con éxito. 
                             Adjunta encontrarás la autorización que debes imprimir o solicitar impresa en tu centro, firmar y entregar como tarde el día de la actividad. Recuerda que es imprescindible para poder participar.
                             La persona responsable de {n} durante la convivencia será {cc}. Si no tienes su contacto puedes pedirlo en tu centro de JMV.
-                            Además, puedes unirte al grupo de WhatsApp de familiares de los participantes en <a href="">este enlace</a>.
-                            Nos vemos el día 15 a las 18:00 en Puerta de Toledo.
+                            Además, puedes unirte al canal de WhatsApp para familiares de participantes en <a href="https://chat.whatsapp.com/IRwa34YMR57Jkzk2PNvm8Q">este enlace</a>.
+                            Nos vemos el día 15 de marzo a las 18:00 en <a href="https://maps.app.goo.gl/59Dwre6TKvULsRbo6">calle Bailén, 43</a>.
                             Muchas gracias,
                             <br> El equipo organizador de la Prepascua
                             JMV - Área 12+9
